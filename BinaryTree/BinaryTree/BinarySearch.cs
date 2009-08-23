be510f0f88baa38d8d2e645f568405d052f5bb14
Xerxes Battiwalla <xbattiwalla@cochlear.com>
2009-08-23 21:51:05 +1000

    Another rewrite of binary search using shifted bounds.

--- BinaryTree/BinaryTree/BinarySearch.cs
+++ BinaryTree/BinaryTree/BinarySearch.cs
@@ -4,42 +4,42 @@ using System;
  * 
  * order of operations when calculating the mid!! subtraction should happen before division, moron!
  * array index offsets (-1). when? where?
+ * Middle of the segment needs to be determined based on the upper and lower bounds, NOT length of array
+ * Dont need to set the upper/lower bound for each criteria - 
+ *      only update the upper bound when number in lower segment
+ *      only update the lower bound (and offset) when number in upper segment
 */
 
 namespace BinaryTree
 {
     public class BinarySearch
     {
+        private const int ItemNotFound = -1;
+
         public int Chop(int valueToFind, int[] dataToSearch)
         {
+            var positionInArray = ItemNotFound;
             var lowerBound = 0;
             var upperBound = dataToSearch.Length - 1;
-            var valuePosition = -1;
-            var itemOffset = 0;
+            var offset = 0;
 
-            if (dataToSearch.Length == 0) return -1;
-
-            do
+            while (positionInArray == -1 && lowerBound <= upperBound)
             {
-                var mid = ((upperBound - lowerBound) / 2) + itemOffset;
+                var mid = ((upperBound - lowerBound) / 2) + offset;
 
-                if (valueToFind == dataToSearch[mid]) valuePosition = mid;
+                if (valueToFind == dataToSearch[mid]) positionInArray = mid;
 
                 if (valueToFind < dataToSearch[mid])
                 {
-                    lowerBound = itemOffset;
                     upperBound = mid - 1;
                 }
                 else if (valueToFind > dataToSearch[mid])
                 {
-                    itemOffset = mid + 1;
-                    lowerBound = mid + 1;
-                    upperBound = dataToSearch.Length - 1;
+                    offset = lowerBound = mid + 1;
                 }
+            }
 
-            } while (valuePosition == -1 && lowerBound <= upperBound);
-
-            return valuePosition;
+            return positionInArray;
         }
     }
 }
--- BinaryTree/BinaryTree/BinaryTreeTest.cs
+++ BinaryTree/BinaryTree/BinaryTreeTest.cs
@@ -6,73 +6,152 @@ using NUnit.Framework;
 namespace BinaryTree 
 {
     [TestFixture]
-    public class BinaryTreeTest 
-    {
-        BinarySearch searchEngine;
+    public class Tester {
+        [Test]
+        public void Test0() {
+            assert_equal(-1, chop(3, new int[0]));
+        }
+
+        [Test]
+        public void Test1() {
+            assert_equal(-1, chop(3, new[] { 1 }));
+        }
+
+        [Test]
+        public void Test2() {
+            assert_equal(0, chop(1, new[] { 1 }));
+        }
+
+        [Test]
+        public void Test3() {
+            assert_equal(0, chop(1, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test4() {
+            assert_equal(1, chop(3, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test5() {
+            assert_equal(2, chop(5, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test6() {
+            assert_equal(-1, chop(0, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test7() {
+            assert_equal(-1, chop(2, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test8() {
+            assert_equal(-1, chop(4, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test9() {
+            assert_equal(-1, chop(6, new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void Test10() {
+            assert_equal(0, chop(1, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test11() {
+            assert_equal(1, chop(3, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test12() {
+            assert_equal(2, chop(5, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test13() {
+            assert_equal(3, chop(7, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test14() {
+            assert_equal(-1, chop(0, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test15() {
+            assert_equal(-1, chop(2, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test16() {
+            assert_equal(-1, chop(4, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test17() {
+            assert_equal(-1, chop(6, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void Test18() {
+            assert_equal(-1, chop(8, new[] { 1, 3, 5, 7 }));
+        }
+
+        [Test]
+        public void TestArrayWithMultipleSearchSegments()
+        {
+            assert_equal(0, chop(1, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(1, chop(3, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(2, chop(5, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(3, chop(7, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(4, chop(9, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(5, chop(11, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(6, chop(13, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(0, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(2, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(4, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(6, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(8, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(10, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(12, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+            assert_equal(-1, chop(14, new[] { 1, 3, 5, 7, 9, 11, 13 }));
+
+            assert_equal(0, chop(1, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(1, chop(3, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(2, chop(5, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(3, chop(7, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(4, chop(9, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(5, chop(11, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(6, chop(13, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(0, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(2, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(4, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(6, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(8, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(10, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(12, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+            assert_equal(-1, chop(14, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
+        }
+
+        private BinarySearch _searchEngine;
 
         [SetUp]
         public void SetUp()
         {
-            searchEngine = new BinarySearch();
+            _searchEngine = new BinarySearch();
         }
 
-        [Test]
-        public void DefaultTests()
-        {
-            Assert.AreEqual(-1, searchEngine.Chop(3, new int[0]));
-            Assert.AreEqual(-1, searchEngine.Chop(3, new [] {1}));
-            Assert.AreEqual(0, searchEngine.Chop(1, new [] {1}));
-
-            Assert.AreEqual(0, searchEngine.Chop(1, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(1, searchEngine.Chop(3, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(2, searchEngine.Chop(5, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(3, searchEngine.Chop(7, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(-1, searchEngine.Chop(0, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(-1, searchEngine.Chop(2, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(-1, searchEngine.Chop(4, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(-1, searchEngine.Chop(6, new[] { 1, 3, 5, 7 }));
-            Assert.AreEqual(-1, searchEngine.Chop(8, new[] { 1, 3, 5, 7 }));
-
-            Assert.AreEqual(0, searchEngine.Chop(1, new [] {1, 3, 5}));
-            Assert.AreEqual(1, searchEngine.Chop(3, new [] {1, 3, 5}));
-            Assert.AreEqual(2, searchEngine.Chop(5, new [] {1, 3, 5}));
-            Assert.AreEqual(-1, searchEngine.Chop(0, new [] {1, 3, 5}));
-            Assert.AreEqual(-1, searchEngine.Chop(2, new [] {1, 3, 5}));
-            Assert.AreEqual(-1, searchEngine.Chop(4, new [] {1, 3, 5}));
-            Assert.AreEqual(-1, searchEngine.Chop(6, new [] {1, 3, 5}));
-
-            Assert.AreEqual(0, searchEngine.Chop(1, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(1, searchEngine.Chop(3, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(2, searchEngine.Chop(5, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(3, searchEngine.Chop(7, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(4, searchEngine.Chop(9, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(5, searchEngine.Chop(11, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(6, searchEngine.Chop(13, new[] { 1, 3, 5, 7, 9, 11, 13}));
-            Assert.AreEqual(-1, searchEngine.Chop(0, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(2, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(4, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(6, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(8, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(10, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(12, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-            Assert.AreEqual(-1, searchEngine.Chop(14, new[] { 1, 3, 5, 7, 9, 11, 13 }));
-
-            Assert.AreEqual(0, searchEngine.Chop(1, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(1, searchEngine.Chop(3, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(2, searchEngine.Chop(5, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(3, searchEngine.Chop(7, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(4, searchEngine.Chop(9, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(5, searchEngine.Chop(11, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(6, searchEngine.Chop(13, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(0, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(2, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(4, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(6, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(8, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(10, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(12, new[] { 1, 3, 5, 7, 9, 11, 13, 15 }));
-            Assert.AreEqual(-1, searchEngine.Chop(14, new[] { 1, 3, 5, 7, 9, 11, 13, 15}));
+        private int chop(int find, int[] array) {
+            return _searchEngine.Chop(find, array);
         }
 
+        private void assert_equal(int expected, int actual) {
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }

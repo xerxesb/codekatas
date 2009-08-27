0fbf8ace753f408b0e972b593e2b6a03dd2d0354
Xerxes Battiwalla <xbattiwalla@cochlear.com>
2009-08-27 13:57:43 +1000

    Created TreeNode Search

---
+++ BinaryTree/TreeNodeSearch.cs
@@ -0,0 +1,9 @@
+namespace BinaryTree
+{
+    public class TreeNodeSearch
+    {
+        //This implementation will be a b-tree search.
+        // first pass will build a tree
+        // second pass will search the tree.
+    }
+}
